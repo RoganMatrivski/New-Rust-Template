997e79f62d494726743c419e4289fe129cbdd508
Robin Mauritz <robinmauritzrm@gmail.com>
2023-09-16 23:23:32 +0700

    Initial commit

---
+++ src/main.rs
@@ -0,0 +1,16 @@
+use color_eyre::Report;
+{%- if use_init %}
+
+mod init;
+{% endif %}
+
+#[tracing::instrument]
+fn main() -> Result<(), Report> {
+    {%- if use_init -%}
+    init::initialize()?;
+    
+    {% endif %}
+    println!("Hello, world!");
+
+    Ok(())
+}
