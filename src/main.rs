3d294b248867d448fda5d57de26b01ffe749200b
Rogan Matrivski L. <robinmauritzrm@gmail.com>
2025-01-02 23:23:57 +0700

    feat: Update main.rs template
    Should've fixed that pesky newline and whitespaces

--- src/main.rs
+++ src/main.rs
@@ -2,13 +2,12 @@ use color_eyre::Report;
 {%- if use_init %}
 
 mod init;
-{% endif %}
+{%- endif %}
 
 #[tracing::instrument]
 fn main() -> Result<(), Report> {
-    {%- if use_init -%}
+    {%- if use_init %}
     init::initialize()?;
-    
     {% endif %}
     println!("Hello, world!");
 
